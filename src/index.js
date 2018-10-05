f7ae756db5a4e94e1b04dcac3bb67623011658e8
garnich <garnich@tut.by>
2018-10-05 22:23:03 +0300

    stringmathod

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 class SmartCalculator {
   constructor(initialValue) {
+    this.initialValue = initialValue.toString();
     // your implementation
   }
 
   add(number) {
+    this.initialValue+='+'+number;
+    return this;
     // your implementation
   }
   
   subtract(number) {
+    this.initialValue+='-'+number;
+    return this;
     // your implementation
   }
 
   multiply(number) {
+    this.initialValue+='*'+number;
+    return this;
     // your implementation
   }
 
   devide(number) {
+    this.initialValue+='/'+number;
+    return this;
     // your implementation
   }
 
   pow(number) {
+    this.initialValue+='**'+number;
+    return this;
     // your implementation
+  }
+  valueOf(){
+    return eval(this.initialValue);
   }
 }
 
